41f18e5cacb1aab0f98fe24f1a565ab4d442e270
Wesley <wesquirk@gmail.com>
2026-02-23 13:17:41 -0800

    added get request to call fetchEvents in the eventRoutes

--- backend/routes/eventRoutes.ts
+++ backend/routes/eventRoutes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { checkPSK } from '../middleware/checkPSK';
-import { ingestEvents } from '../controllers/eventsController';
+import { ingestEvents, fetchEvents } from '../controllers/eventsController';
 
 
 //initialize router
 const router = express.Router();
 
 //define routes
+router.get('/events', checkPSK, fetchEvents);
 router.post('/events', checkPSK, ingestEvents);
 
 export default router;
